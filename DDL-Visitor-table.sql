f3ed7256ff2107f2a995047895f587cbc19e5b9e
AndreeaaBadea <andreea.badea@capgemini.com>
2022-03-14 08:59:46 +0200

    Added price and visitor tables

---
+++ DDL-Visitor-table.sql
@@ -0,0 +1,9 @@
+//------------------------------------ CREATE table visitor ----------------------------------
+
+
+ CREATE TABLE visitor (
+    id_visitor INT NOT NULL AUTO_INCREMENT,
+    type VARCHAR(20),
+    id_price INT NOT NULL,
+    ADD CONSTRAINT `fk_id_price` FOREIGN KEY (`id_price`) REFERENCES `zoo`.`price` (`id_price`)
+   );
